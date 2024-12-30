1dd025ed65d49c20b1bed9647cfa26d5145d71f0
DjordjeProgramer <djordje.kuzmanovic289@gmail.com>
2024-12-30 08:42:02 +0100

    Delete DomainName.js

--- DomainName.js
+++
@@ -1,9 +0,0 @@
-function changeLanguage(language) {
-    
-    const newPath = `/${language}`;
-
-    history.pushState({}, '', newPath);
-  
-    console.log('New path:', window.location.pathname);
-  }
-  
